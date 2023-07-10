8f70b41a68a57aa732d818aeab64484a8de6e3b7
Prashant Jadhav <120619315+Prashant0520@users.noreply.github.com>
2023-07-10 23:30:32 +0530

    Add files via upload

---
+++ Even-Odd.py
@@ -0,0 +1,17 @@
+# Program to print Even and Odd no. from [1-N]
+
+#Even No.
+n=int(input("Enter Number to print "))
+i=1
+while(i<=n):
+    if i%2==0:
+        print(i)
+    i=i+1
+
+#Odd No.
+n=int(input("Enter Number to print"))
+i=1
+while(i<=n):
+    if i%2!=0:
+        print(i)
+    i=i+1
